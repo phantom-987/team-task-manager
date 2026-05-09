28c1009e434c57e2a5231bdbe4eacd1cb8b7f30d
Shantanu <shantanubiswas5555@gmail.com>
2026-05-09 11:29:35 +0530

    Fix frontend API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,25 +1,32 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
   return config
 })
 
 api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
       localStorage.removeItem('token')
       localStorage.removeItem('user')
       window.location.href = '/login'
     }
-    return Promise.reject(err)
+
+    return Promise.reject(error)
   }
 )
 
